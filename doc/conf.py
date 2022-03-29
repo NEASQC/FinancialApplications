228828e0faabd94b3782e029480d08d3d1d0f6fc
gonfeco <gonfeco@gmail.com>
2022-03-29 19:11:35 +0200

    Changes to be committed: 	modified:   doc/conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -3,13 +3,12 @@
 
 import os
 import sys
-import numpy as np
 from sphinx.ext.autodoc import between
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- General configuration ---------------------------------------------------
 
