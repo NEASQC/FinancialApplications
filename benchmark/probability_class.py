03aee3743aa834b89e6051c51c9084b657b5691e
gonfeco <gonfeco@gmail.com>
2022-07-04 12:28:53 +0200

    Changes to be committed: 	new file:   benchmark/01_Benchamark_Finanzas.ipynb 	new file:   benchmark/02_Benchmark_Finanzas.ipynb 	new file:   benchmark/dictionary_combination.py 	new file:   benchmark/finance_benchmark.py 	new file:   benchmark/jsons/cqpeae_configuration.json 	new file:   benchmark/jsons/density_probability.json 	new file:   benchmark/jsons/domain_configuration.json 	new file:   benchmark/jsons/iqae_configuration.json 	new file:   benchmark/jsons/iqpeae_configuration.json 	new file:   benchmark/jsons/json_data.json 	new file:   benchmark/jsons/mlae_configuration.json 	new file:   benchmark/jsons/payoffs.json 	new file:   benchmark/jsons/rqae_configuration.json 	new file:   benchmark/jsons/test_density_probability.json 	new file:   benchmark/jsons/test_domain_configuration.json 	new file:   benchmark/jsons/test_payoffs.json 	new file:   benchmark/payoff_class.py 	new file:   benchmark/probability_class.py

---
+++ benchmark/probability_class.py
@@ -0,0 +1,81 @@
+
+import QQuantLib.utils.classical_finance as cf
+from functools import partial
+
+def text_is_none(variable, variable_name, variable_type = float):
+    if variable is None:
+        message = variable_name+' argument is None. Some '+str(variable_type)+' should be  provided'
+        raise ValueError(message)
+
+
+class DensityProbability:
+
+    """
+    Class for selectin pay off functions
+    algorithm
+    """
+
+    def __init__(self, probability_type: str, **kwargs):
+        """
+
+        Method for initializing the class
+
+        Parameters
+        ----------
+
+        probability_type : string
+           type of probability density function to load
+        """
+
+
+        self.probability_type = probability_type
+        text_is_none(self.probability_type, 'probability_type', variable_type=str)
+        self.probability = None
+        self.density_probability = None
+        self.get_density(**kwargs)
+        self.get_density_prob(**kwargs)
+
+    def get_density(self, **kwargs):
+
+        if self.probability_type == 'Black-Scholes':
+
+            s_0 = kwargs.get('s_0', None)
+            text_is_none(s_0, 's_0', variable_type=float)
+            risk_free_rate = kwargs.get('risk_free_rate', None)
+            text_is_none(risk_free_rate, 'risk_free_rate', variable_type=float)
+            maturity = kwargs.get('maturity', None)
+            text_is_none(maturity, 'maturity', variable_type=float)
+            volatility = kwargs.get('volatility', None)
+            text_is_none(volatility, 'volatility', variable_type=float)
+
+            self.probability = partial(
+                cf.bs_probability,
+                s_0=s_0,
+                risk_free_rate=risk_free_rate,
+                volatility=volatility,
+                maturity=maturity
+            )
+        else:
+            raise ValueError()
+    def get_density_prob(self, **kwargs):
+
+        if self.probability_type == 'Black-Scholes':
+
+            s_0 = kwargs.get('s_0', None)
+            text_is_none(s_0, 's_0', variable_type=float)
+            risk_free_rate = kwargs.get('risk_free_rate', None)
+            text_is_none(risk_free_rate, 'risk_free_rate', variable_type=float)
+            maturity = kwargs.get('maturity', None)
+            text_is_none(maturity, 'maturity', variable_type=float)
+            volatility = kwargs.get('volatility', None)
+            text_is_none(volatility, 'volatility', variable_type=float)
+
+            self.density_probability = partial(
+                cf.bs_density,
+                s_0=s_0,
+                risk_free_rate=risk_free_rate,
+                volatility=volatility,
+                maturity=maturity
+            )
+        else:
+            raise ValueError()
