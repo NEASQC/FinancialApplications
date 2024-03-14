e9fc6dd20a68cc558b4753a5c0de08d87bdad463
gonfeco <gonfeco@gmail.com>
2024-03-14 16:21:18 +0100

    Deleted repetition loop in ae_price_estimation.py

--- QQuantLib/finance/ae_price_estimation.py
+++ QQuantLib/finance/ae_price_estimation.py
@@ -74,84 +74,75 @@ def ae_price_estimation(**kwargs):
     if po.pay_off_bs is not None:
         exact_solution = po.pay_off_bs(**ae_problem)
 
-    lista = []
-    #For doing several repetitions
-    for i in range(ae_problem["number_of_tests"]):
-        #Each loop step solves a complete price estimation problem
+    #Now we update the input dictionary with the probability and the
+    #function arrays
+    ae_problem.update({
+        "array_function" : norm_pay_off,
+        "array_probability" : norm_p_x,
+    })
 
-        #Now we update the input dictionary with the probability and the
-        #function arrays
-        ae_problem.update({
-            "array_function" : norm_pay_off,
-            "array_probability" : norm_p_x,
-        })
+    #EXECUTE COMPUTATION
+    solution, solver_object = q_solve_integral(**ae_problem)
 
-        #EXECUTE COMPUTATION
-        solution, solver_object = q_solve_integral(**ae_problem)
+    #For generating the output DataFrame we delete the arrays
+    del ae_problem["array_function"]
+    del ae_problem["array_probability"]
 
-        #For generating the output DataFrame we delete the arrays
-        del ae_problem["array_function"]
-        del ae_problem["array_probability"]
+    #Undoing the normalisations
+    ae_expectation = solution * pay_off_normalisation * p_x_normalisation
 
-        #Undoing the normalisations
-        ae_expectation = solution * pay_off_normalisation * p_x_normalisation
+    #Creating the output DataFrame with the complete information
 
-        #Creating the output DataFrame with the complete information
+    #The basis will be the input python dictionary for traceability
+    pdf = pd.DataFrame([ae_problem])
+    #Added normalisation constants
+    pdf["payoff_normalisation"] = pay_off_normalisation
+    pdf["p_x_normalisation"] = p_x_normalisation
 
-        #The basis will be the input python dictionary for traceability
-        pdf = pd.DataFrame([ae_problem])
-        #Added normalisation constants
-        pdf["payoff_normalisation"] = pay_off_normalisation
-        pdf["p_x_normalisation"] = p_x_normalisation
-
-        #Expectation calculation using Riemann sum
-        pdf["riemann_expectation"] = np.sum(p_x * pay_off)
-        #Expectation calculation using AE integration techniques
-        pdf[
-            [col + "_expectation" for col in ae_expectation.columns]
-        ] = ae_expectation
-        # Pure integration Absolute Error
-        pdf["absolute_error"] = np.abs(
-            pdf["ae_expectation"] - pdf["riemann_expectation"])
-        # Finance Info
-        #Exact option price under the Black-Scholes model
-        pdf["finance_exact_price"] = exact_solution
-        #Option price estimation using expectation computed as Riemann sum
-        pdf["finance_riemann_price"] = pdf["riemann_expectation"] * np.exp(
-            -pdf["risk_free_rate"] * pdf["maturity"]
-        )
-        #Option price estimation using expectation computed by AE integration
-        pdf["finance_price_estimation"] = pdf["ae_expectation"] * \
-            np.exp(-pdf["risk_free_rate"] * pdf["maturity"]).iloc[0]
-        #Computing Absolute with discount: Rieman vs AE techniques
-        pdf["finance_error_riemann"] = np.abs(
-            pdf["finance_price_estimation"] - pdf["finance_riemann_price"]
-        )
-        #Computing Absolute error: Exact BS price vs AE techniques
-        pdf["finance_error_exact"] = np.abs(
-            pdf["finance_price_estimation"] - pdf["finance_exact_price"])
-        #Other interesting staff
-        if solver_object is None:
-            #Computation Fails Encoding 0 and RQAE
+    #Expectation calculation using Riemann sum
+    pdf["riemann_expectation"] = np.sum(p_x * pay_off)
+    #Expectation calculation using AE integration techniques
+    pdf[
+        [col + "_expectation" for col in ae_expectation.columns]
+    ] = ae_expectation
+    # Pure integration Absolute Error
+    pdf["absolute_error"] = np.abs(
+        pdf["ae_expectation"] - pdf["riemann_expectation"])
+    pdf["measured_epsilon"] = np.abs(
+        pdf["ae_u_expectation"] - pdf["ae_l_expectation"]) / 2.0
+    # Finance Info
+    #Exact option price under the Black-Scholes model
+    pdf["finance_exact_price"] = exact_solution
+    #Option price estimation using expectation computed as Riemann sum
+    pdf["finance_riemann_price"] = pdf["riemann_expectation"] * np.exp(
+        -pdf["risk_free_rate"] * pdf["maturity"]
+    )
+    #Option price estimation using expectation computed by AE integration
+    pdf["finance_price_estimation"] = pdf["ae_expectation"] * \
+        np.exp(-pdf["risk_free_rate"] * pdf["maturity"]).iloc[0]
+    #Computing Absolute with discount: Rieman vs AE techniques
+    pdf["finance_error_riemann"] = np.abs(
+        pdf["finance_price_estimation"] - pdf["finance_riemann_price"]
+    )
+    #Computing Absolute error: Exact BS price vs AE techniques
+    pdf["finance_error_exact"] = np.abs(
+        pdf["finance_price_estimation"] - pdf["finance_exact_price"])
+    #Other interesting staff
+    if solver_object is None:
+        #Computation Fails Encoding 0 and RQAE
+        pdf["schedule_pdf"] = [None]
+        pdf["oracle_calls"] = [None]
+        pdf["max_oracle_depth"] = [None]
+        pdf["circuit_stasts"] = [None]
+        pdf["run_time"] = [None]
+    else:
+        if solver_object.schedule_pdf is None:
             pdf["schedule_pdf"] = [None]
-            pdf["oracle_calls"] = [None]
-            pdf["max_oracle_depth"] = [None]
-            pdf["circuit_stasts"] = [None]
-            pdf["run_time"] = [None]
         else:
-            if solver_object.schedule_pdf is None:
-                pdf["schedule_pdf"] = [None]
-            else:
-                pdf["schedule_pdf"] = [solver_object.schedule_pdf.to_dict()]
-            pdf["oracle_calls"] = solver_object.oracle_calls
-            pdf["max_oracle_depth"] = solver_object.max_oracle_depth
-            pdf["circuit_stasts"] = [solver_object.solver_ae.circuit_statistics]
-            pdf["run_time"] = solver_object.solver_ae.run_time
+            pdf["schedule_pdf"] = [solver_object.schedule_pdf.to_dict()]
+        pdf["oracle_calls"] = solver_object.oracle_calls
+        pdf["max_oracle_depth"] = solver_object.max_oracle_depth
+        pdf["circuit_stasts"] = [solver_object.solver_ae.circuit_statistics]
+        pdf["run_time"] = solver_object.solver_ae.run_time
 
-        #Saving pdf
-        if ae_problem["save"]:
-            with open(ae_problem["file_name"], "a") as f_pointer:
-                pdf.to_csv(f_pointer, mode="a", header=f_pointer.tell() == 0, sep = ";")
-        lista.append(pdf)
-    complete_pdf = pd.concat(lista)
-    return complete_pdf
+    return pdf
